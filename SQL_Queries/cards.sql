1e9ed7a7d9e2222339acb334b0680b7a9a5142ce
Markus Rösner <markus.roesner@gmx.net>
2023-12-11 17:39:20 +0100

    Add SQL queries for cards, user_cards, and users

---
+++ SQL_Queries/cards.sql
@@ -0,0 +1,9 @@
+SELECT * FROM cards;
+	
+	
+INSERT INTO cards (name, damage, element, type, id) VALUES 
+('WaterGoblin', 10.0, 'water', 'monster', '845f0dc7-37d0-426e-994e-43fc3ac83c08'),
+('Dragon', 50.0, 'normal', 'monster', '99f8f8dc-e25e-4a95-aa2c-782823f36e2a'),
+('WaterSpell', 20.0, 'water', 'spell', 'e85e3976-7c86-4d06-9a80-641c2019a79f'),
+('Ork', 45.0, 'normal', 'monster', '1cb6ab86-bdb2-47e5-b6e4-68c5ab389334'),
+('FireSpell', 25.0, 'fire', 'spell', 'dfdd758f-649c-40f9-ba3a-8657f4b3439f');
---
+++ SQL_Queries/user_cards.sql
@@ -0,0 +1,3 @@
+SELECT * FROM user_cards;
+
+SELECT * FROM user_cards WHERE userid = 1 AND indeck = true;
