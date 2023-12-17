1d2ab0955134c49a548d325cdeb6d2aa6f867deb
Markus Rösner <markus.roesner@gmx.net>
2023-12-17 18:53:03 +0100

    Add userRepository test cases

--- MonsterTradingCardsGame.Tests/src/Repositories/UserRepositoryTests.cs
+++ MonsterTradingCardsGame.Tests/src/Repositories/UserRepositoryTests.cs
@@ -1,12 +1,127 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MonsterTradingCardsGame.Repositories;
+using MonsterTradingCardsGame.Server;
+using MonsterTradingCardsGame.Interfaces;
+using MonsterTradingCardsGame.Models;
+using MonsterTradingCardsGame.Services.Interfaces;
+using Moq;
 
 namespace MonsterTradingCardsGame.Tests.Repositories
 {
+
+
+
+    [TestFixture]
     internal class UserRepositoryTests
     {
+
+        private Mock<IUserRepository> _mockUserRepository;
+        private Mock<IAuthenticationService> _mockAuthService;
+        private Mock<ICardRepository> _mockCardRepository;
+        private Mock<IHttpServerEventArguments> _mockHttpEventArguments;
+        private UserRepository _userRepository;
+
+        //Setup for the tests
+        [SetUp]
+        public void Setup()
+        {
+            _mockUserRepository = new Mock<IUserRepository>();
+            _mockAuthService = new Mock<IAuthenticationService>();
+            _mockCardRepository = new Mock<ICardRepository>();
+            _mockHttpEventArguments = new Mock<IHttpServerEventArguments>();
+
+            _userRepository = new UserRepository(_mockCardRepository.Object);
+        }
+
+        [Test]
+        public void GetUserByUsername_ExistingUsername_ReturnsUser()
+        {
+            // Arrange
+            string username = "kienboec";
+
+            // Act
+            User? user = _userRepository.GetUserByUsername(username);
+
+            // Assert
+            Assert.IsNotNull(user);
+            Assert.That(user.Username, Is.EqualTo(username));
+        }
+
+        [Test]
+        public void GetUserByUsername_NonExistingUsername_ReturnsNull()
+        {
+            // Arrange
+            string username = "nonexistinguser";
+
+            // Act
+            User? user = _userRepository.GetUserByUsername(username);
+
+            // Assert
+            Assert.IsNull(user);
+        }
+
+        //test for update user
+        [Test]
+        public void UpdateUser_ExistingUser_ReturnsTrue()
+        {
+            // Arrange
+            User user = new User()
+            {
+                Username = "kienboec",
+                Name = "Kienboec",
+                Bio = "I am a cool guy",
+                Image = ":----D",
+                Coins = 100,
+                Elo = 1000
+            };
+
+            // Act
+            bool result = _userRepository.Update(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void UpdateNonExistingUser_ReturnsFalse()
+        {
+            // Arrange
+            User user = new User()
+            {
+                Username = "nonexistinguser",
+                Name = "Kienboec",
+                Bio = "I am a cool guy",
+                Image = ":----D",
+                Coins = 100,
+                Elo = 1000
+            };
+
+            // Act
+            bool result = _userRepository.Update(user);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        //test for register user
+        [Test]
+        public void RegisterUser_ReturnsTrue()
+        {
+            // Arrange
+            User user = new User()
+            {
+                Username = "newUser",
+                Password = "12345678"
+            };
+
+            // Act
+            bool result = _userRepository.Save(user);
+
+            // Assert
+            Assert.IsTrue(result);
+
+            // Cleanup
+            _userRepository.Delete(user);
+        }
+
     }
 }
