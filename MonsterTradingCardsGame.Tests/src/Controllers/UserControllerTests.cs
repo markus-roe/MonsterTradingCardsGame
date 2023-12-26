91c3c9b0796a04148a25b4ac3d4cbe964efc9732
Markus Rösner <markus.roesner@gmx.net>
2023-12-26 13:10:19 +0100

    Login and GetUser test

--- MonsterTradingCardsGame.Tests/src/Controllers/UserControllerTests.cs
+++ MonsterTradingCardsGame.Tests/src/Controllers/UserControllerTests.cs
@@ -5,6 +5,7 @@ using MonsterTradingCardsGame.Models;
 using System.Text.Json;
 using MonsterTradingCardsGame.Services.Interfaces;
 using MonsterTradingCardsGame.Server;
+using MonsterTradingCardsGame.Utils.UserProfile;
 
 
 namespace MonsterTradingCardsGame.Tests.Controllers
@@ -172,6 +173,88 @@ namespace MonsterTradingCardsGame.Tests.Controllers
 
         }
 
+        //login with wrong username
+        [Test]
+        public void UserController_LoginUser_WrongUsername()
+        {
+            // Arrange
+            string username = "testuser";
+            string password = "testpassword";
+
+            User user = new User()
+            {
+                Username = username,
+                Password = password
+            };
+
+            _mockUserRepository = new Mock<IUserRepository>();
+            _mockAuthService = new Mock<IAuthenticationService>();
+            _mockHttpEventArguments = new Mock<IHttpServerEventArguments>();
+
+            _mockHttpEventArguments.Setup(m => m.Method).Returns("POST");
+            _mockHttpEventArguments.Setup(m => m.Path).Returns("/session");
+            _mockHttpEventArguments.Setup(m => m.Payload).Returns(JsonSerializer.Serialize(user));
+
+            _userController = new UserController(
+                _mockUserRepository.Object,
+                _mockAuthService.Object,
+                _mockCardRepository.Object);
+
+            // Act
+            _userController.Login(_mockHttpEventArguments.Object);
+
+            // Assert
+            _mockHttpEventArguments.Verify(m => m.Reply(401, "Invalid username/password provided"), Times.Once());
+
+        }
+
+        //get user
+        [Test]
+        public void UserController_GetUser()
+        {
+            // Arrange
+            User user = new User()
+            {
+                Username = "testuser",
+                Password = "testpassword",
+                Name = "testuser",
+                Bio = "testbio",
+                Image = "testimage"
+            };
+
+            UserProfile userProfile = new()
+            {
+                Name = user.Name,
+                Bio = user.Bio,
+                Image = user.Image
+            };
+
+            _mockUserRepository = new Mock<IUserRepository>();
+            _mockAuthService = new Mock<IAuthenticationService>();
+            _mockHttpEventArguments = new Mock<IHttpServerEventArguments>();
+
+            _mockUserRepository.Setup(m => m.GetUserByUsername(user.Username)).Returns(user);
+
+            _mockHttpEventArguments.Setup(m => m.Method).Returns("GET");
+            _mockHttpEventArguments.Setup(m => m.User).Returns(user);
+            _mockHttpEventArguments.Setup(m => m.Path).Returns("/users/testuser");
+            _mockHttpEventArguments.Setup(m => m.Parameters).Returns(new Dictionary<string, string> { { "username", "testuser" } });
+
+
+            _userController = new UserController(
+                _mockUserRepository.Object,
+                _mockAuthService.Object,
+                _mockCardRepository.Object);
+
+            // Act
+            _userController.GetUserByUsername(_mockHttpEventArguments.Object);
+
+
+            // Assert
+            string response = JsonSerializer.Serialize(userProfile);
+
+            _mockHttpEventArguments.Verify(m => m.Reply(200, response), Times.Once());
+        }
     }
 
 }
