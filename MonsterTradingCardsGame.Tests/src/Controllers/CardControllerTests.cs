228b0e350b6408362fd925df6a43963fb9ddc41f
Markus Rösner <markus.roesner@gmx.net>
2023-12-28 18:32:34 +0100

    Add test for creating a package in CardController

--- MonsterTradingCardsGame.Tests/src/Controllers/CardControllerTests.cs
+++ MonsterTradingCardsGame.Tests/src/Controllers/CardControllerTests.cs
@@ -30,6 +30,35 @@ namespace MonsterTradingCardsGame.Tests.Controllers
                 _mockUserRepository.Object);
         }
 
+        //admin creates new packages
+        [Test]
+        public void CardController_CreatePackage()
+        {
+
+            string payload = "[{\"Id\":\"845f0dc7-37d0-426e-994e-43fc3ac83c08\", \"Name\":\"WaterGoblin\", \"Damage\": 10.0}, {\"Id\":\"99f8f8dc-e25e-4a95-aa2c-782823f36e2a\", \"Name\":\"Dragon\", \"Damage\": 50.0}, {\"Id\":\"e85e3976-7c86-4d06-9a80-641c2019a79f\", \"Name\":\"WaterSpell\", \"Damage\": 20.0}, {\"Id\":\"1cb6ab86-bdb2-47e5-b6e4-68c5ab389334\", \"Name\":\"Ork\", \"Damage\": 45.0}, {\"Id\":\"dfdd758f-649c-40f9-ba3a-8657f4b3439f\", \"Name\":\"FireSpell\",    \"Damage\": 25.0}]";
+
+            User user = new User()
+            {
+                Username = "admin"
+            };
+
+            //mock http event arguments
+            _mockHttpEventArguments.Setup(m => m.Method).Returns("POST");
+            _mockHttpEventArguments.Setup(m => m.Path).Returns("/packages");
+            _mockHttpEventArguments.Setup(m => m.Payload).Returns(payload);
+            _mockHttpEventArguments.Setup(m => m.User).Returns(user);
+
+            //mock card repository
+            _mockCardRepository.Setup(m => m.GetCardById(It.IsAny<string>())).Returns((Card)null);
+            _mockCardRepository.Setup(m => m.SavePackage(It.IsAny<List<Card>>())).Returns(true);
+
+            // Act
+            _cardController.CreatePackage(_mockHttpEventArguments.Object);
+
+            // Assert
+            _mockHttpEventArguments.Verify(m => m.Reply(200, "Package and cards successfully created"), Times.Once());
+
+        }
     }
 
 }
