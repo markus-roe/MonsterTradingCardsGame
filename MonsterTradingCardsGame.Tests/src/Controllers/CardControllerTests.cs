b267f5f1a5658d082f1221705db1a252ccf5a2a9
Markus Rösner <markus.roesner@gmx.net>
2023-12-28 18:33:01 +0100

    Add test for creating a package with existing cards

--- MonsterTradingCardsGame.Tests/src/Controllers/CardControllerTests.cs
+++ MonsterTradingCardsGame.Tests/src/Controllers/CardControllerTests.cs
@@ -134,6 +134,31 @@ namespace MonsterTradingCardsGame.Tests.Controllers
             _mockHttpEventArguments.Verify(m => m.Reply(400, "You need to provide 5 cards"), Times.Once());
         }
 
+        //at least one card in the package already exists
+        [Test]
+        public void CardController_CreatePackage_CardAlreadyExists()
+        {
+
+            string payload = "[{\"Id\":\"845f0dc7-37d0-426e-994e-43fc3ac83c08\", \"Name\":\"WaterGoblin\", \"Damage\": 10.0}, {\"Id\":\"99f8f8dc-e25e-4a95-aa2c-782823f36e2a\", \"Name\":\"Dragon\", \"Damage\": 50.0}, {\"Id\":\"e85e3976-7c86-4d06-9a80-641c2019a79f\", \"Name\":\"WaterSpell\", \"Damage\": 20.0}, {\"Id\":\"1cb6ab86-bdb2-47e5-b6e4-68c5ab389334\", \"Name\":\"Ork\", \"Damage\": 45.0}, {\"Id\":\"dfdd758f-649c-40f9-ba3a-8657f4b3439f\", \"Name\":\"FireSpell\",    \"Damage\": 25.0}]";
+
+            //mock http event arguments
+            _mockHttpEventArguments.Setup(m => m.Method).Returns("POST");
+            _mockHttpEventArguments.Setup(m => m.Path).Returns("/packages");
+            _mockHttpEventArguments.Setup(m => m.Payload).Returns(payload);
+            _mockHttpEventArguments.Setup(m => m.User).Returns(new User() { Username = "admin" });
+
+            //mock card repository
+            _mockCardRepository.Setup(m => m.GetCardById(It.IsAny<string>())).Returns(new Card());
+            _mockCardRepository.Setup(m => m.SavePackage(It.IsAny<List<Card>>())).Returns(true);
+
+            // Act
+            _cardController.CreatePackage(_mockHttpEventArguments.Object);
+
+            // Assert
+            _mockHttpEventArguments.Verify(m => m.Reply(409, "At least one card in the packages already exists"), Times.Once());
+
+        }
+
     }
 
 }
