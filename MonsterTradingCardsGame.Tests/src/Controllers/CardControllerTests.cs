39d260caab009d6eec6319f4c4d96f61ec6af902
Markus Rösner <markus.roesner@gmx.net>
2023-12-28 18:32:40 +0100

    Add non-admin package creation test case

--- MonsterTradingCardsGame.Tests/src/Controllers/CardControllerTests.cs
+++ MonsterTradingCardsGame.Tests/src/Controllers/CardControllerTests.cs
@@ -59,6 +59,32 @@ namespace MonsterTradingCardsGame.Tests.Controllers
             _mockHttpEventArguments.Verify(m => m.Reply(200, "Package and cards successfully created"), Times.Once());
 
         }
+
+        //non admin tries to create new packages
+        [Test]
+        public void CardController_CreatePackage_NonAdmin()
+        {
+
+            string payload = "[{\"Id\":\"845f0dc7-37d0-426e-994e-43fc3ac83c08\", \"Name\":\"WaterGoblin\", \"Damage\": 10.0}, {\"Id\":\"99f8f8dc-e25e-4a95-aa2c-782823f36e2a\", \"Name\":\"Dragon\", \"Damage\": 50.0}, {\"Id\":\"e85e3976-7c86-4d06-9a80-641c2019a79f\", \"Name\":\"WaterSpell\", \"Damage\": 20.0}, {\"Id\":\"1cb6ab86-bdb2-47e5-b6e4-68c5ab389334\", \"Name\":\"Ork\", \"Damage\": 45.0}, {\"Id\":\"dfdd758f-649c-40f9-ba3a-8657f4b3439f\", \"Name\":\"FireSpell\",    \"Damage\": 25.0}]";
+
+            //mock http event arguments
+            _mockHttpEventArguments.Setup(m => m.Method).Returns("POST");
+            _mockHttpEventArguments.Setup(m => m.Path).Returns("/packages");
+            _mockHttpEventArguments.Setup(m => m.Payload).Returns(payload);
+            _mockHttpEventArguments.Setup(m => m.User).Returns(new User() { Username = "notanadmin" });
+
+            //mock card repository
+            _mockCardRepository.Setup(m => m.GetCardById(It.IsAny<string>())).Returns((Card)null);
+            _mockCardRepository.Setup(m => m.SavePackage(It.IsAny<List<Card>>())).Returns(true);
+
+            // Act
+            _cardController.CreatePackage(_mockHttpEventArguments.Object);
+
+            // Assert
+            _mockHttpEventArguments.Verify(m => m.Reply(403, "You are not allowed to create packages"), Times.Once());
+
+        }
+
     }
 
 }
